02b2993aefa605b2535e46b2002f9fe3399298c2
g0mteng <gomteng03@naver.com>
2024-11-14 20:24:42 +0900

    nested route 에러 수정

--- router.jsx
+++ router.jsx
@@ -1,21 +1,22 @@
 import { Route, Routes, Navigate } from 'react-router-dom'
+import { Header } from '@/components/templates'
 
-export const Router = () => {
+export const AppRoutes = () => {
   return (
     <Routes>
       <Route path='/' element={<Navigate to='/main' replace />} />
-      <Route path='/main' element={<div />}>
-        <Route path='/' element={<div />} />
-        <Route path='/search' element={<div />} />
+      <Route path='/main' element={<Header />}>
+        <Route index element={<div />} />
+        <Route path='search' element={<div />} />
       </Route>
-      <Route path='/auth' element={<div />}>
-        <Route path='/sign-in' element={<div />} />
-        <Route path='/sign-up' element={<div />} />
+      <Route path='/auth' element={<Header />}>
+        <Route path='sign-in' element={<div />} />
+        <Route path='sign-up' element={<div />} />
       </Route>
-      <Route path='/content' element={<div />}>
-        <Route path='/write' element={<div />} />
-        <Route path='/detail/:mailId' element={<div />} />
-        <Route path='/read/:userId' element={<div />} />
+      <Route path='/content' element={<Header />}>
+        <Route path='write' element={<div />} />
+        <Route path='detail/:mailId' element={<div />} />
+        <Route path='read/:userId' element={<div />} />
       </Route>
     </Routes>
   )
