d97078522cb15c23da791367ea02b78e4d3a6a98
g0mteng <gomteng03@naver.com>
2024-11-16 13:53:02 +0900

    index 페이지 개발

--- src/apis/index.js
+++ src/apis/index.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 import { filters } from '@/constants'
 
 const api = axios.create({
-  baseURL: import.meta.env.DEV ? 'http://localhost:5173/' : import.meta.env.VITE_BASEURL,
+  // baseURL: import.meta.env.DEV ? 'http://localhost:5173/' : import.meta.env.VITE_BASEURL,
+  baseURL: import.meta.env.VITE_BASEURL,
 })
 
 export const getTree = async (id) => {
